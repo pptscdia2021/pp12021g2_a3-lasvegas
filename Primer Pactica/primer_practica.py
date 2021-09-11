153ab2c5a9b5838a51c2b27ceacd8eb54bff27c7
darkguardia <32149800+darkguardia@users.noreply.github.com>
2021-09-11 15:53:51 -0300

    Casi terminado

---
+++ Primer Pactica/primer_practica.py
@@ -0,0 +1,6 @@
+import os
+import producto.py
+
+if __name__ == "__main__":
+    print("Primer programa en Python")
+    print("El producto es: ", str(producto()))
---
+++ Primer Pactica/producto.py
@@ -0,0 +1,2 @@
+def producto(a,b,c,d):
+    return (a*b*c*d)
