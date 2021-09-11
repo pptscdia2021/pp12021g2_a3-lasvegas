517b7a56316d8d2ba7ab7be1afce5151d8a945d4
darkguardia <32149800+darkguardia@users.noreply.github.com>
2021-09-11 16:13:19 -0300

    Funcion producto completada y lista para llevar a rama principal

--- primer_practica/primer_practica.py
+++ primer_practica/primer_practica.py
@@ -1,6 +1,6 @@
 import os
-import primer_practica.producto.py
+from producto import producto
 
 if __name__ == "__main__":
     print("Primer programa en Python")
-    print("El producto es: ", str(producto()))
+    print("El producto es: ", str(producto(2,3,4,5)))
