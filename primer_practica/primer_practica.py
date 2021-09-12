0518861e3d3cc7018d06eb7bb18206583f32f02d
gimebinaghi <gimenabinaghi@hotmail.com>
2021-09-12 16:43:57 -0300

    Creación función resta y rama 2

--- primer_practica/primer_practica.py
+++ primer_practica/primer_practica.py
@@ -1,6 +1,8 @@
 import os
 from producto import producto
+from resta import resta
 
 if __name__ == "__main__":
     print("Primer programa en Python")
     print("El producto es: ", str(producto(2,3,4,5)))
+    print("La resta es: ", str(producto(2,3)))
