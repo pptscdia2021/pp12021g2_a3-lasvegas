10d63100692b651c2a53696103bd6b4813ad9e9e
gimebinaghi <gimenabinaghi@hotmail.com>
2021-09-12 16:56:56 -0300

    Mi función resta

--- primer_practica/primer_practica.py
+++ primer_practica/primer_practica.py
@@ -5,4 +5,4 @@ from resta import resta
 if __name__ == "__main__":
     print("Primer programa en Python")
     print("El producto es: ", str(producto(2,3,4,5)))
-    print("La resta es: ", str(producto(2,3)))
+    print("La resta es: ", str(resta(2,3)))
