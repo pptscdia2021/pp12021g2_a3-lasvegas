68bffe0e25f0cf20a37f56af7f1e95d6b5c05530
darkguardia <32149800+darkguardia@users.noreply.github.com>
2021-09-19 13:04:28 -0300

    Rama de objetivo 1

---
+++ segunda_practica/web_scraping_bolsa.py
@@ -0,0 +1,13 @@
+# Importando librerias
+#
+# Si es necesario: pip install BeautifulSoup
+# Si es necesario: pip install datetime
+
+import requests
+from bs4 import BeautifulSoup
+import csv
+from datetime import datetime
+
+# Indicar la URL
+url_page = 'http://www.bolsamadrid.es/esp/aspx/Indices/Resumen.aspx'
+
