bc38f1d0cc01c4a34d7afce1b3e6e64bcc4ef4eb
darkguardia <32149800+darkguardia@users.noreply.github.com>
2021-10-03 17:10:37 -0300

    Yahoo finance

--- segunda_practica/scrap_yahoo_finance.py
+++ segunda_practica/scrap_yahoo_finance.py
@@ -1,4 +1,14 @@
 import yfinance as yf
 
-def syahoo():
-    
+Amazon = yf.Ticker("IBEX")
+print(Amazon.info) 
+
+print(Amazon.info['ask']) #Precio al que compro una acción
+print(Amazon.info['bid']) #Precio al que vendo una acción
+
+df = yf.download("IBEX", start="2021-09-03", end="2021-09-04",group_by="ticker") 
+print(df)
+
+ticker = yf.Ticker('IBEX')
+aapl_df = ticker.history(period="5y")
+aapl_df['Close'].plot(title="APPLE's stock price")
