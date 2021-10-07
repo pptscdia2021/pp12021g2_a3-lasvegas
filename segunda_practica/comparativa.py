a428e281b37ae465ea4c3925a7e45454e212d9c5
leivahm <86030916+leivahm@users.noreply.github.com>
2021-10-07 17:08:08 -0300

    modificaciones de comparativa

---
+++ segunda_practica/comparativa.py
@@ -0,0 +1,19 @@
+# Crear la función comparativa y check
+from matplotlib.lines import _LineStyle
+import matplotlib.pyplot as plt
+
+x = [2016, 2017, 2018, 2019, 2020, 2021]
+y = [45, 38, 47, 40, 44, 30]
+
+plt.plot(x,y, marker = 'o', linestyle = '--')
+plt.show()
+
+"""
+def comparativa(a,b):
+    print(a)
+    print(b)
+
+def grafik(a,b):
+    plt.plot(a,b)
+    plt.show()
+    """
--- segunda_practica/main.py
+++ segunda_practica/main.py
@@ -1,11 +1,11 @@
 from web_scraping_bolsa import scraping
 from Objetivo2 import maxYMin
 from scrap_yahoo_finance import scrapYahoo
+from comparativa import comparativa
 
 accionesYahoo = ["ANA.MC", "BBVA.MC", "GRF.MC", "ELE.MC","REP.MC"]
 
 if __name__ == "__main__":
     df1 = scraping()
     df2 = scrapYahoo(accionesYahoo)
-    print(df1)
-    print(df2)
+    comparativa(df1,df2)
