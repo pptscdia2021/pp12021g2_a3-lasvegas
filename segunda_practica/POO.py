1c6fc506105331b1dec0ad010708d119faac8589
darkguardia <32149800+darkguardia@users.noreply.github.com>
2021-11-05 23:48:00 -0300

    Tarea de POO completa

--- segunda_practica/POO.py
+++ segunda_practica/POO.py
@@ -1,6 +1,8 @@
 import requests
 import re
 import pandas as pd
+import matplotlib.pyplot as plt
+import yfinance as yf
 from bs4 import BeautifulSoup
 from datetime import datetime
 
@@ -46,12 +48,19 @@ class acciones:
             df['Ticker'][i] = re.sub(r"\s+", "", (ticker +'.MC'))
             df['URL'][i] = self.url_page
             i = i+1
-        
         return df
 
-    def scrap_accion(self):
-        # Scrap de la accion, le pasamos el ticker
-        pass
+    def scrap_accion(self,accion):
+        df_list = list()
+
+        for ticker in accion:
+            data = yf.download(ticker, group_by='ticker', period='1d')
+            data['ticker'] = ticker  
+            df_list.append(data)
+    
+        df = pd.concat(df_list)
+        print(df)
+
 
     def guardar(self,df):
         self.df = df
@@ -61,12 +70,44 @@ class acciones:
     def leer(self):
         # Leer el CSV
         opendf = pd.read_csv('exportacion.csv')
-        
-        return opendf
+        print(opendf)
 
-    def graficar(self):
-        # Graficar el estado de la accion, le pasamos el ticker
-        pass
+    def listado_acciones(self):
+        # Lee el CSV
+        opendf = pd.read_csv('exportacion.csv')
+
+        # Crea una lista con los tickers
+        lista = opendf['Ticker']
+        print(lista)
+
+    def graficar(self,df,catidad):
+        print(df)
+        tiko = df['Ticker']
+
+        i = 0
+        while i < catidad:
+            tic = yf.Ticker(tiko[i])
+            dibuDf = tic.history(period = "5y")
+            dibuDf['Close'].plot(title = tiko[i])
+            i = i + 1
+            plt.show()
 
 tarea = acciones()
-tarea.leer()
+
+# Scraping por aaciones
+# tarea.scrap_accion(['AAPL','MSFT','GOOG','FB'])
+
+# Graficar 2 desde el Scraping
+# tarea.graficar(tarea.scrap_madrid(tarea.url_page))
+
+# Graficar 2 aaccion
+# tarea.graficar({'Ticker':['ANA.MC','AENA.MC']},2)
+
+# Graficar 1 aaccion
+#tarea.graficar({'Ticker':['ANA.MC']},1)
+
+# Leer el CSV
+# tarea.leer()
+
+# Listado de acciones
+# tarea.listado_acciones()
